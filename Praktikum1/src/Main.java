786f2c2f9caf213ed527c60ee83f9bbc78c8cb8a
Client <client@clientcrash.com>
2025-04-25 00:16:43 +0200

    added custom file pick

--- Praktikum1/src/Main.java
+++ Praktikum1/src/Main.java
@@ -1,24 +1,104 @@
-import java.awt.BorderLayout;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class Main {
-    static class Frame extends JFrame {
+    static class Game extends JFrame {
+        File wordListFile;
         BorderLayout root = new BorderLayout();
-        Canvas canvas = new Canvas();
+        Canvas canvas;
+        WordList wordList;
+        boolean gameOver = false;
+        int guesses = 0;
+        JTextField input = new JTextField();
+        JButton restart = new JButton("Restart");
+        JButton eigeneWortListeAuswaehlen = new JButton("Eigene wortliste Auswáhlen");
 
-        Frame() {
-            super();
+
+        void guess(String word) {
+            if (word.length() == 0 || gameOver) return;
+            char guess = word.charAt(0);
+            boolean result = wordList.guessChar(guess);
+            if (!result) guesses++;
+            if(guesses > 12 || wordList.hadBeenGuessed()) {
+                gameOver = true;
+            }
+
+            canvas.repaint(); // update display
+        }
+
+        Game() {
+            super("Hangman");
+
+            // Initialize Word List
+            wordList = new WordList();
+            wordList.pickRandomWord();
+            Assets.load();
 
             setBounds(0, 0, 800, 500);
             setLayout(root);
             setResizable(false);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
+            canvas = new Canvas();
+            canvas.setSize(800, 500);
+            add(canvas, BorderLayout.CENTER);
 
-            add(canvas);
+            input.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(gameOver) return;
+                    guess(input.getText());
+                    input.setText("");
+                }
+            });
+            restart.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    guesses = 0;
+                    if(wordListFile != null){
+                        wordList = new WordList(wordListFile);
+                    } else {
+                        wordList = new WordList();
+                    }
+                    gameOver = false;
+                    canvas.repaint();
+                }
+            });
+            JFrame frame = this;
+            eigeneWortListeAuswaehlen.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    FileDialog dia = new FileDialog(frame, "Liste Auswählen", FileDialog.LOAD);
+                    dia.setFilenameFilter(new FilenameFilter() {
+                                @Override
+                                public boolean accept(File dir, String name) {
+                                    return name.endsWith(".txt");
+                                }
+                            });
+                    dia.setVisible(true);
+                    File file = new File(dia.getFile());
+                    wordList = new WordList(file);
+                    wordListFile = file;
+                    guesses = 0;
+                    gameOver = false;
+                    canvas.repaint();
+                }
+            });
+
+            JPanel bottom = new JPanel();
+            bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+            bottom.add(input, BorderLayout.WEST);
+            bottom.add(eigeneWortListeAuswaehlen, BorderLayout.CENTER);
+            bottom.add(restart, BorderLayout.EAST);
+
+
+            add(bottom, BorderLayout.SOUTH);
 
             setVisible(true);
         }
@@ -27,14 +107,65 @@ public class Main {
             Canvas() {
                 super();
             }
-            void draw() {
-                Graphics g = getGraphics();
-                // do stuff
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.white);
+                g.fillRect(0,0,getWidth(),getHeight());
+                g.setFont(g.getFont().deriveFont(30f));
+
+                int i = 0;
+                for (char c : wordList.guessedChars) {
+                    i++;
+                    if (wordList.getWord().toLowerCase().contains(String.valueOf(Character.toLowerCase(c)))) {
+                        g.setColor(Color.GREEN);
+                    } else {
+                        g.setColor(Color.RED);
+                    }
+                    if(Character.isUpperCase(c)) continue;
+                    g.drawString(String.valueOf(c), i * 30, 350);
+                }
+                g.setColor(Color.BLACK);
+                char[] chars = wordList.getMaskedWord().toCharArray();
+                for (i=0; i < wordList.getMaskedWord().length(); i++) {
+                    // draw underscore under every character
+                    g.drawString("_", 440+30*i,205);
+
+                    // draw the guessed word parts
+                    if (chars[i] != '_') g.drawString(String.valueOf(chars[i]), 440+30*(i%20),200+(i/20)*60); // next line after 20 chars
+                }
+
+                g.drawImage(Assets.hangman[guesses], 0,0, null);
+
+                if (gameOver) {
+                    g.setFont(g.getFont().deriveFont(60f));
+                    if (wordList.hadBeenGuessed()) {
+                        g.setColor(Color.green);
+                        g.drawString("Victory", 200, 100);
+                    } else {
+                        g.setColor(Color.red);
+                        g.drawString("Gameover", 150,100);
+                    }
+                }
             }
         }
-    }    
+    }
+    public class Assets {
+        public static BufferedImage[] hangman = new BufferedImage[14];
+        public static void load() {
+            try {
+                for (int i = 0; i < hangman.length; i++) {
+                    hangman[i] = ImageIO.read(new File("Praktikum1/assets/"+(i+1)+".png"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        new Frame();
+        String version = System.getProperty("java.version");
+        System.out.println(version);
+        new Game();
     }
 }
--- Praktikum1/src/WordList.java
+++ Praktikum1/src/WordList.java
@@ -1,8 +1,89 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class WordList {
-    List<String> possibleWords;
-    
+    public List<String> possibleWords;
+    public int pickedWordIndex;
+    public List<Character> guessedChars;
+    public ArrayList<Character> alphabetList = new ArrayList<>(Arrays.asList(
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+    ));
+
+    public WordList() {
+        possibleWords = new ArrayList<String>();
+        guessedChars = new ArrayList<>();
+        possibleWords.add("Haus");
+        possibleWords.add("Baum");
+    }
+
+    public WordList(File file){
+        possibleWords = new ArrayList<String>();
+        guessedChars = new ArrayList<>();
+        if(file.exists() && file.isFile() && file.canRead()){
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    possibleWords.add(line);
+                }
+            } catch (IOException e){
+                System.out.println(e);
+            }
+        } else {
+            System.err.println("Error: File is not readable");
+        }
+    }
+
+    public void pickRandomWord() {
+        Random random = new Random();
+        pickedWordIndex = random.nextInt(possibleWords.size());
+    }
+
+    public String getMaskedWord() {
+        String word = possibleWords.get(pickedWordIndex);
+        StringBuilder maskedWord = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            if (guessedChars.contains(Character.toLowerCase(c))) {
+                maskedWord.append(c);
+            } else {
+                maskedWord.append('_');
+            }
+        }
+
+        return maskedWord.toString();
+    }
+
+
+    public String getWord() {
+        return possibleWords.get(pickedWordIndex);
+    }
+
+    public boolean guessChar(Character c) {
+        if(guessedChars.contains(c)) return true;
+
+        if(Character.isUpperCase(c)){
+            guessedChars.add(Character.toLowerCase(c));
+        } else {
+            guessedChars.add(c);
+        }
+        return possibleWords.get(pickedWordIndex).contains(String.valueOf(c));
+    }
+
+    public boolean hadBeenGuessed(){
+        for (Character c : getWord().toLowerCase().toCharArray()) {
+            if(!guessedChars.contains(c)) return false;
+        }
+        return true;
+    }
+
 
 
 }
