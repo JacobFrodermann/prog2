140a2221ba0a6784359543c8d956c384c9805f0b
Client <client@clientcrash.com>
2025-04-25 10:37:56 +0200

    Update WordList.java

--- Praktikum1/src/WordList.java
+++ Praktikum1/src/WordList.java
@@ -11,11 +11,6 @@ public class WordList {
     public List<String> possibleWords;
     public int pickedWordIndex;
     public List<Character> guessedChars;
-    public ArrayList<Character> alphabetList = new ArrayList<>(Arrays.asList(
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y', 'z'
-    ));
 
     public WordList() {
         possibleWords = new ArrayList<String>();
